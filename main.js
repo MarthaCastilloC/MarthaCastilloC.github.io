f9568fd8849e2c29e14bf2f0122983679c4ae677
MarthaCastilloC <162236833+MarthaCastilloC@users.noreply.github.com>
2024-03-04 19:15:03 -0600

    commitInicial

---
+++ main.js
@@ -0,0 +1,13 @@
+function ShowHide(){
+    var latinoamerica = document.getElementsByClassName("latinoamerica")[0];
+
+    if(latinoamerica.style.visibility=="hidden"){
+        latinoamerica.style.visibility="visiblr";
+
+    }
+
+else{
+    latinoamerica.style.visibility="hidden"
+
+}
+}
